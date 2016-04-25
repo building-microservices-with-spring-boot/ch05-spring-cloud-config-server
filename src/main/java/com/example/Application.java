90b5ba6fd6fc53ef20e1030d933bcfa4dafd650b
Engin Yöyen <engin.yoyen@gmail.com>
2016-04-25 08:37:27 +0200

    Initial commit

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@EnableConfigServer
+@ComponentScan
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
